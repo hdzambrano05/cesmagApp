8a068d83dae05e6248954fafca1178d0fc4ecc7e
Harold <hdzambrano.2350@unicesmag.edu.co>
2023-11-01 07:36:10 -0500

    version-1.1

---
+++ facultades.php
@@ -0,0 +1,42 @@
+<?php
+include 'index.php';
+include 'conexion.php';
+?>
+
+<h1> Listado de facultades </h1>
+
+<div align="right">
+  <a class= "btn btn-primary"> Adiccionar</a>
+</div>
+
+<table class="table">
+  <thead>
+    <tr>
+      <th scope="col">Codigo</th>
+      <th scope="col">Nombre</th>
+      <th scope="col">Opciones</th>
+    </tr>
+  </thead>
+  <tbody>
+  <?php
+        $sql = "SELECT * FROM facultades";
+        $result = $conn->query($sql);
+
+        if ($result->num_rows > 0) {
+            while($row = $result->fetch_assoc()) {
+                echo "<tr>";
+                echo "<td>".$row["Codigo_fac"]."</td>";
+                echo "<td>".$row["Nombre_fac"]."</td>";
+                echo "<td>
+                    <a class='btn btn-success'>Editar</a>
+                    <a class='btn btn-danger'>Eliminar</a>
+                    </td>";
+                echo "</tr>";
+            }
+        } else {
+            echo "0 resultados";
+        }
+        $conn->close();
+    ?>
+  </tbody>
+</table>
---
+++ index.php
@@ -0,0 +1,43 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Cesmag App</title>
+    <link rel="stylesheet" href="css/mdb.min.css">
+    <script src="js/mdb.min.js"></script>
+    <script src="https://unpkg.com/@popperjs/core@2/dist/umd/popper.js"></script>
+</head>
+<body>
+    <nav class="navbar navbar-expand-lg navbar-light bg-light">
+    <div class="container-fluid">
+        <a class="navbar-brand" href="#">Cesmag App</a>
+        <button
+        class="navbar-toggler"
+        type="button"
+        data-mdb-toggle="collapse"
+        data-mdb-target="#navbarNav"
+        aria-controls="navbarNav"
+        aria-expanded="false"
+        aria-label="Toggle navigation"
+        >
+        <i class="fas fa-bars"></i>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarNav">
+        <ul class="navbar-nav">
+            <li class="nav-item">
+                <a class="nav-link active" aria-current="page" href="index.php">Inicio</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="facultades.php">Facultades</a>
+            </li>
+                <li class="nav-item">
+            <a class="nav-link" href="programas.php">Programas</a>
+            </li>
+        </ul>
+        </div>
+    </div>
+    </nav>
+
+</body>
+</html>
